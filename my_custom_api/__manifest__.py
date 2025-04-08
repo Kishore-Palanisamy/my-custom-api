fd259bb76c8e420b51952523cd844bac43e62c71
Kishore-iOS <kishore@fuzionest.com>
2025-04-08 10:29:45 +0530

    icon, cover image and html description added

--- my_custom_api/__manifest__.py
+++ my_custom_api/__manifest__.py
@@ -2,7 +2,17 @@
     'name': 'My Custom API',
     'version': '17.0.1.0.0',
     'summary': 'Provides custom JSON API endpoints for reporting',
-    'description': 'This module offers APIs like /api/hello, /api/top-customers, etc.',
+    'description': 
+        """
+            <h3>My Custom API Module</h3>
+            <p>This module provides the following API endpoints:</p>
+            <ul>
+            <li><strong>/api/hello</strong> - Basic hello-world check</li>
+            <li><strong>/api/top-customers</strong> - Top 10 customers by sales</li>
+            <li><strong>/api/sales-total</strong> - Total sales in a date range</li>
+            <li><strong>/api/low-stock</strong> - Products with low stock</li>
+            </ul>
+        """,
     'author': 'Kishore',
     'category': 'Sales/API',
     'depends': ['base', 'sale_management', 'stock', 'sale'],
